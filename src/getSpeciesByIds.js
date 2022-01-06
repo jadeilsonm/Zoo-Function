04007a5dbf89aa4ce4c907b2f5742969f5523758
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-06 18:51:54 -0300

    dev: requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) return [];
+  return species.filter((specie) => ids.some((i) => i === specie.id));
 }
 
 module.exports = getSpeciesByIds;
