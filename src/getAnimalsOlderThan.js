993c06a55ed55e2d391568966f107e9fa7cf8561
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-06 19:33:26 -0300

    dev: requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { name } = require('faker');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { residents } = data.species.find((specie) => specie.name === animal);
+  return residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
