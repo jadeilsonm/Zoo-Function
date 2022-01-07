15dad2833a0db67641ec0370154d4d54d0288239
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-07 18:10:13 -0300

    requisto 4: passando nos teste 1 - 3

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.some((ids) => ids === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const arrFiltrado = employees.filter((employee) => {
+    const { managers } = employee;
+    return managers.some((Element) => Element === managerId);
+  });
+  return arrFiltrado.reduce((acc, curr) => [...acc, `${curr.firstName} ${curr.lastName}`], []);
 }
-
+getRelatedEmployees('a200');
 module.exports = { isManager, getRelatedEmployees };
