b62531a6ed680191a2bfe8121176f433f61706e9
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-08 14:11:00 -0300

    dev: requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const obj = {};
+    return (species.reduce((acc, curr) => {
+      // console.log('objeto', obj);
+      const { name, residents } = curr;
+      obj[`${name}`] = residents.length;
+      return obj;
+    }, obj));
+  }
+  if (!animal.sex) {
+    return species.find((Element) => Element.name === animal.specie).residents.length;
+  }
+  return species.find((Element) => Element.name === animal.specie).residents.filter((Element) => Element.sex === animal.sex).length;
 }
+
+// console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
