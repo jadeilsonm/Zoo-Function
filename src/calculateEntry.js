cd14946910926a128f1fda1c58007377f2c83cf8
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-08 23:56:32 -0300

    dev: requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const base = { child: 0, adult: 0, senior: 0 };
+  return entrants.reduce((acc, curr) => {
+    if (curr.age < 18) {
+      acc.child += 1;
+    } else if (curr.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, base);
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const result = countEntrants(entrants);
+  if (result.length === 0) return 0;
+  const { child, adult, senior } = result;
+  const { child: childPrice, adult: adultPrice, senior:seniorPrice } = prices;
+  return child * childPrice + adult * adultPrice + senior * seniorPrice;
 }
-
 module.exports = { calculateEntry, countEntrants };
