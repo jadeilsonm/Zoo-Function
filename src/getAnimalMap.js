c981659f77419a5e76ba8868f09f8b756c35bc80
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-10 16:08:12 -0300

    dev: requisito 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const regions = species.reduce((acc, curr) => {
+  const { location: local } = curr;
+  if (!acc[local]) acc[local] = [];
+  return acc;
+}, {});
+
+const todosNomes = () => species.reduce((acc, curr) => {
+  const { location: local, name } = curr;
+  acc[local].push(name);
+  return acc;
+}, regions);
+
+const incluidName = () => species.reduce((acc, curr) => {
+  const { location: local, name: nome, residents } = curr;
+  const names = residents.reduce((ac, cur) => [...ac, cur.name], []);
+  acc[local].push({ [nome]: names });
+  return acc;
+}, regions);
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options) {
+    const { sex, sorted, includeNames } = options;
+    if (sex === 'female' || sex === 'female' && sorted === true) return todosNomes();
+    if (includeNames === true) {
+      return incluidName();
+    }
+  }
+  return todosNomes();
 }
-
+// console.log(getAnimalMap({ sex: 'female' }));
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const semParametros = () =>
+  Object.keys(hours).reduce((acc, curr) => {
+    const { open, close } = hours[curr];
+    const list = [];
+    species.forEach((Element) => {
+      if (Element.availability.includes(curr)) list.push(Element.name);
+    });
+    if (curr !== 'Monday') {
+      acc[curr] = { officeHour: `Open from ${open}am until ${close}pm`, exhibition: list };
+    }
+    if (curr === 'Monday') {
+      acc[curr] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    }
+    return acc;
+  }, {});
+
+const getAnimais = (name) => species.find((el) => el.name === name).availability;
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const Dias = Object.keys(hours);
+  const Animais = [];
+  species.forEach((Element) => {
+    Animais.push(Element.name);
+  });
+  if (!scheduleTarget || (!Animais.includes(scheduleTarget) && !Dias.includes(scheduleTarget))) {
+    return semParametros();
+  }
+  if (Dias.includes(scheduleTarget)) {
+    const days = semParametros();
+    const obj = {};
+    obj[scheduleTarget] = days[scheduleTarget];
+    return obj;
+  }
+  return getAnimais(scheduleTarget);
 }
 
 module.exports = getSchedule;
