f2386deef4eea07bfc20776afd8ccf73e9e36c89
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-10 17:39:06 -0300

    dev: requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
+
+const maiorIdade = (name) => {
+  let idade = 0;
+  const resident = species.find((El) => El.id === name).residents;
+  resident.forEach((animal) => {
+    if (animal.age > idade) idade = animal.age;
+  });
+  const animal = resident.find((el) => el.age === idade);
+  return [animal.name, animal.sex, animal.age];
+};
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const verificar = employees.find((ids) => ids.id === id).responsibleFor[0];
+  return (maiorIdade(verificar));
 }
 
 module.exports = getOldestFromFirstSpecies;
