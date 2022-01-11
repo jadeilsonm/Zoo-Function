534376cce795a458051d25e5d29a5c69b9303934
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-01-11 20:48:37 -0300

    Dev: requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,33 +1,44 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-// const regions = species.reduce((acc, curr) => {
-//   const { location: local } = curr;
-//   if (!acc[local]) acc[local] = [];
-//   return acc;
-// }, {});
+const todosEspecies = (regions) => species.reduce((acc, { location: local, name }) => {
+  acc[local].push(name);
+  return acc;
+}, regions);
 
-// const todosNomes = () => species.reduce((acc, curr) => {
-//   const { location: local, name } = curr;
-//   acc[local].push(name);
-//   return acc;
-// }, regions);
+const todosNames = (regions, p = { sex: false }) => species.reduce((acc, curr) => {
+  const { sex, sorted } = p;
+  const { location: local, name: nome, residents } = curr;
+  const names = residents.reduce((ac, cur) => {
+    if (!sex) {
+      ac.push(cur.name);
+      return ac;
+    }
+    if (cur.sex === sex) ac.push(cur.name);
+    return ac;
+  }, []);
+  if (sorted === true) {
+    acc[local].push({ [nome]: names.sort() });
+    return acc;
+  }
+  acc[local].push({ [nome]: names });
+  return acc;
+}, regions);
 
-// const incluidName = () => species.reduce((acc, curr) => {
-//   const { location: local, name: nome, residents } = curr;
-//   const names = residents.reduce((ac, cur) => [...ac, cur.name], []);
-//   acc[local].push({ [nome]: names });
-//   return acc;
-// }, regions);
+const optionsIncud = (regions, options) => {
+  const { sex, sorted } = options;
+  if (sex && sorted) return todosNames(regions, options);
+  if (sorted === true) return todosNames(regions, options);
+  if (sex) return todosNames(regions, options);
+  return todosNames(regions);
+};
 
 function getAnimalMap(options) {
-  // if (options) {
-  //   const { sex, sorted, includeNames } = options;
-  //   if (sex === 'female' || sex === 'female' && sorted === true) return todosNomes();
-  //   if (includeNames === true) {
-  //     return incluidName();
-  //   }
-  // }
-  // return todosNomes();
+  const regions = { NE: [], NW: [], SE: [], SW: [] };
+  if (!options) return todosEspecies(regions);
+  if (options.includeNames) {
+    return optionsIncud(regions, options);
+  }
+  return todosEspecies(regions);
 }
-// console.log(getAnimalMap({ sex: 'female' }));
+
 module.exports = getAnimalMap;
